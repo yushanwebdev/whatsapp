60858a0513bc4f90eb26ca97ae9158f6b5bfe0fd
yushanwebdev <yushanwebdev@gmail.com>
2022-10-16 20:12:36 +0530

    feat: Input Box - Configure the `<KeyboardAvoidingView />` in the `<ChatScreen />` to prevent the Keyboard Overlap

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,18 +1,30 @@
-import { ImageBackground, StyleSheet, FlatList } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+  KeyboardAvoidingView,
+} from "react-native";
 import bg from "../../assets/images/BG.png";
 import messages from "../../assets/data/messages.json";
 import Message from "../components/Message";
+import InputBox from "../components/InputBox";
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList
-        data={messages}
-        renderItem={({ item }) => <Message message={item} />}
-        style={styles.list}
-        inverted
-      />
-    </ImageBackground>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.bg}
+    >
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
